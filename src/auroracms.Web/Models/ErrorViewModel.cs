2edb326e18a234ba0bc056bfb3f6c9824a9618ec
clevertonalmeida <117084721+clevertonalmeida@users.noreply.github.com>
2024-09-19 15:25:14 -0300

    first commit

---
+++ src/auroracms.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace auroracms.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
